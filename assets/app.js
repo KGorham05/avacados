73d81afbfe522e9b72ae8b584379cbd8dcd95f18
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-13 17:48:34 -0700

    testing jquery refactor

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
     console.log('Hello world!');
-//    const scriptURL = 'https://script.google.com/macros/s/AKfycbz9ykRXhZW2BWB2TltdfrAkqneAX-dUrvnZ2yciGLotYIiBTV8/exec'
-//    const form = document.forms['order-details']
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz9ykRXhZW2BWB2TltdfrAkqneAX-dUrvnZ2yciGLotYIiBTV8/exec'
+    const form = $("#order-details")
 
-//    form.addEventListener('submit', e => {
-//        e.preventDefault()
-//        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-//            .then(response => {
-//                console.log('Success!', response)
-//                // empty form fields
-//                $('#num-boxes').val('');
-//                $('#delivery-date').val('');
-//                $('#recurring').val('');
-//                $('#501c3').val('');
-//                $('#contact-name').val('');
-//                $('#contact-phone').val('');
-//                $('#contact-email').val('');
-//                $('#delivery-address').val('');
-//                $('#special-instructions').val('');
-//            })
-//            .catch(error => {
-//                console.error('Error!', error.message);
-//                alert('Oh no... something went wrong! Please try again later!')
-//            })
-//    })
+    form.submit(e => {
+       e.preventDefault()
+       fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+           .then(response => {
+               console.log('Success!', response)
+               // empty form fields
+               $('#num-boxes').val('');
+               $('#delivery-date').val('');
+               $('#recurring').val('');
+               $('#501c3').val('');
+               $('#contact-name').val('');
+               $('#contact-phone').val('');
+               $('#contact-email').val('');
+               $('#delivery-address').val('');
+               $('#special-instructions').val('');
+           })
+           .catch(error => {
+               console.error('Error!', error.message);
+               alert('Oh no... something went wrong! Please try again later!')
+           })
+   })
 });
