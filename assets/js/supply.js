257a5e162c49425300536a638d1fc9a8859c2a6b
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-14 17:24:06 -0700

    started working on supply form page

---
+++ assets/js/supply.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+   console.log('hello world!')
+   // const scriptURL =
+   //   "https://script.google.com/macros/s/AKfycbyPpJ_vPpL92ynCGJoil1SSpwRTkyq-rljoJplhX3Dh2D67MG8/exec";
+   // const form = document.forms["submit-to-google-sheet"];
+ 
+   // form.addEventListener("submit", (e) => {
+   //   e.preventDefault();
+   //   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+   //     .then((response) => {
+   //       console.log("Success!", response);
+   //       alert("Success! Order placed!");
+   //       // empty form fields
+   //          $('#num-boxes').val('');
+   //          $('#delivery-date').val('');
+   //          $('#recurring').val('');
+   //          $('#501c3').val('');
+   //          $('#contact-name').val('');
+   //          $('#contact-phone').val('');
+   //          $('#contact-email').val('');
+   //          $('#delivery-address').val('');
+   //          $('#special-instructions').val('');
+   //     })
+   //     .catch((error) => {
+   //       console.error("Error!", error.message);
+   //       alert("Oh no... something went wrong! Please try again later!");
+   //     });
+   // });
+ });
+ 
+ 
