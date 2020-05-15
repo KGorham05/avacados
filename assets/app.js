e0fe51851c869804233ee844e537ed4aa7954728
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-14 17:06:26 -0700

    form successfully updating the google sheet!

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,29 @@
 $(document).ready(function () {
-    console.log('Hello world!3');
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbz9ykRXhZW2BWB2TltdfrAkqneAX-dUrvnZ2yciGLotYIiBTV8/exec'
-    const form = document.forms['order-details']
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbyPpJ_vPpL92ynCGJoil1SSpwRTkyq-rljoJplhX3Dh2D67MG8/exec";
+  const form = document.forms["submit-to-google-sheet"];
 
-    form.addEventListener('submit', e => {
-       e.preventDefault()
-       fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-           .then(response => {
-               console.log('Success!', response)
-               alert('Success! Order placed!');
-               // empty form fields
-               $('#num-boxes').val('');
-               $('#delivery-date').val('');
-               $('#recurring').val('');
-               $('#501c3').val('');
-               $('#contact-name').val('');
-               $('#contact-phone').val('');
-               $('#contact-email').val('');
-               $('#delivery-address').val('');
-               $('#special-instructions').val('');
-           })
-           .catch(error => {
-               console.error('Error!', error.message);
-               alert('Oh no... something went wrong! Please try again later!')
-           })
-   })
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then((response) => {
+        console.log("Success!", response);
+        alert("Success! Order placed!");
+        // empty form fields
+        //    $('#num-boxes').val('');
+        //    $('#delivery-date').val('');
+        //    $('#recurring').val('');
+        //    $('#501c3').val('');
+        //    $('#contact-name').val('');
+        //    $('#contact-phone').val('');
+        //    $('#contact-email').val('');
+        //    $('#delivery-address').val('');
+        //    $('#special-instructions').val('');
+      })
+      .catch((error) => {
+        console.error("Error!", error.message);
+        alert("Oh no... something went wrong! Please try again later!");
+      });
+  });
 });
+
