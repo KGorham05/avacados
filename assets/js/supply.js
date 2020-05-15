3b78c8b795cd09e9c7379631f0eed35ccccaee92
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-14 18:21:02 -0700

    minor style and text changes, supply form posting to google sheet

--- assets/js/supply.js
+++ assets/js/supply.js
@@ -1,31 +1,39 @@
 $(document).ready(function () {
-   console.log('hello world!')
-   // const scriptURL =
-   //   "https://script.google.com/macros/s/AKfycbyPpJ_vPpL92ynCGJoil1SSpwRTkyq-rljoJplhX3Dh2D67MG8/exec";
-   // const form = document.forms["submit-to-google-sheet"];
+   const scriptURL =
+     "https://script.google.com/macros/s/AKfycbxxNVJM3pHP-uQRJsXN7Tf3-Xo1-HzPwmsXL16CnRND2lUqnsfq/exec";
+   const form = document.forms["submit-to-google-sheet"];
  
-   // form.addEventListener("submit", (e) => {
-   //   e.preventDefault();
-   //   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-   //     .then((response) => {
-   //       console.log("Success!", response);
-   //       alert("Success! Order placed!");
-   //       // empty form fields
-   //          $('#num-boxes').val('');
-   //          $('#delivery-date').val('');
-   //          $('#recurring').val('');
-   //          $('#501c3').val('');
-   //          $('#contact-name').val('');
-   //          $('#contact-phone').val('');
-   //          $('#contact-email').val('');
-   //          $('#delivery-address').val('');
-   //          $('#special-instructions').val('');
-   //     })
-   //     .catch((error) => {
-   //       console.error("Error!", error.message);
-   //       alert("Oh no... something went wrong! Please try again later!");
-   //     });
-   // });
+   form.addEventListener("submit", (e) => {
+     if ($("#company-name").val().trim() === "") {
+       alert("Please enter your company name");
+       return;
+     }
+     e.preventDefault();
+     fetch(scriptURL, { method: "POST", body: new FormData(form) })
+       .then((response) => {
+         console.log("Success!", response);
+         alert("Success! Thank you for your offering! Someone will reach out to you soon.");
+         // empty form fields
+            $('#company-name').val('');
+            $('#contact-name').val('');
+            $('#phone').val('');
+            $('#email').val('');
+            $('#produce-offering').val('');
+            $('#full-load-pricing').val('');
+            $('#pack-config').val('');
+            $('#total-load-qty').val('');
+            $('#cost-per-truck').val('');
+            $('#cost-per-lb').val('');
+            $('#num-loads-now').val('');
+            $('#num-loads-next-week').val('');
+            $('#date-of-offering').val('');
+            $('#valid-through-date').val('');
+       })
+       .catch((error) => {
+         console.error("Error!", error.message);
+         alert("Oh no... something went wrong! Please try again later!");
+       });
+   });
  });
  
  
