2884dbd7f76d5653d87cc3f19ca7fc51b8e1350a
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-20 16:54:47 -0700

    added section about program being put on hold

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,29 @@
-$(document).ready(function () {
-  const scriptURL =
-    "https://script.google.com/macros/s/AKfycbyPpJ_vPpL92ynCGJoil1SSpwRTkyq-rljoJplhX3Dh2D67MG8/exec";
-  const form = document.forms["submit-to-google-sheet"];
+// $(document).ready(function () {
+//   const scriptURL =
+//     "https://script.google.com/macros/s/AKfycbyPpJ_vPpL92ynCGJoil1SSpwRTkyq-rljoJplhX3Dh2D67MG8/exec";
+//   const form = document.forms["submit-to-google-sheet"];
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch(scriptURL, { method: "POST", body: new FormData(form) })
-      .then((response) => {
-        console.log("Success!", response);
-        alert("Success! In order to fulfill your order, we also require you to email a pdf/or jpg document showing your current 501(c)(3) status to our email at farmertofamilies@gmail.com");
-        // empty form fields
-           $('#num-boxes').val('');
-           $('#delivery-date').val('');
-           $('#recurring').val('');
-           $('#501c3').val('');
-           $('#contact-name').val('');
-           $('#contact-phone').val('');
-           $('#contact-email').val('');
-           $('#delivery-address').val('');
-           $('#special-instructions').val('');
-           $('#org-name').val('');
-      })
-      .catch((error) => {
-        console.error("Error!", error.message);
-        alert("Oh no... something went wrong! Please try again later!");
-      });
-  });
-});
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//       .then((response) => {
+//         console.log("Success!", response);
+//         alert("Success! In order to fulfill your order, we also require you to email a pdf/or jpg document showing your current 501(c)(3) status to our email at farmertofamilies@gmail.com");
+//         // empty form fields
+//            $('#num-boxes').val('');
+//            $('#delivery-date').val('');
+//            $('#recurring').val('');
+//            $('#501c3').val('');
+//            $('#contact-name').val('');
+//            $('#contact-phone').val('');
+//            $('#contact-email').val('');
+//            $('#delivery-address').val('');
+//            $('#special-instructions').val('');
+//            $('#org-name').val('');
+//       })
+//       .catch((error) => {
+//         console.error("Error!", error.message);
+//         alert("Oh no... something went wrong! Please try again later!");
+//       });
+//   });
+// });
--- assets/js/supply.js
+++ assets/js/supply.js
@@ -1,42 +1,42 @@
-$(document).ready(function () {
-   const scriptURL =
-     "https://script.google.com/macros/s/AKfycbxxNVJM3pHP-uQRJsXN7Tf3-Xo1-HzPwmsXL16CnRND2lUqnsfq/exec";
-   const form = document.forms["submit-to-google-sheet"];
+// $(document).ready(function () {
+//    const scriptURL =
+//      "https://script.google.com/macros/s/AKfycbxxNVJM3pHP-uQRJsXN7Tf3-Xo1-HzPwmsXL16CnRND2lUqnsfq/exec";
+//    const form = document.forms["submit-to-google-sheet"];
  
-   form.addEventListener("submit", (e) => {
-     if ($("#company-name").val().trim() === "") {
-       alert("Please enter your company name");
-       return;
-     }
-     e.preventDefault();
-     fetch(scriptURL, { method: "POST", body: new FormData(form) })
-       .then((response) => {
-         console.log("Success!", response);
-         alert("Success! Thank you for your offering! Someone will reach out to you soon.");
-         // empty form fields
-            $('#company-name').val('');
-            $('#contact-name').val('');
-            $('#phone').val('');
-            $('#email').val('');
-            $('#produce-offering').val('');
-            $('#full-load-pricing').val('');
-            $('#pack-config').val('');
-            $('#total-load-qty').val('');
-            $('#cost-per-truck').val('');
-            $('#cost-per-lb').val('');
-            $('#num-loads-now').val('');
-            $('#num-loads-next-week').val('');
-            $('#date-of-offering').val('');
-            $('#valid-through-date').val('');
-            $('#opportuinty-produce').val('');
-            $('#shelf-life').val('');
-            $('#box-offering').val('');
-       })
-       .catch((error) => {
-         console.error("Error!", error.message);
-         alert("Oh no... something went wrong! Please try again later!");
-       });
-   });
- });
+//    form.addEventListener("submit", (e) => {
+//      if ($("#company-name").val().trim() === "") {
+//        alert("Please enter your company name");
+//        return;
+//      }
+//      e.preventDefault();
+//      fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//        .then((response) => {
+//          console.log("Success!", response);
+//          alert("Success! Thank you for your offering! Someone will reach out to you soon.");
+//          // empty form fields
+//             $('#company-name').val('');
+//             $('#contact-name').val('');
+//             $('#phone').val('');
+//             $('#email').val('');
+//             $('#produce-offering').val('');
+//             $('#full-load-pricing').val('');
+//             $('#pack-config').val('');
+//             $('#total-load-qty').val('');
+//             $('#cost-per-truck').val('');
+//             $('#cost-per-lb').val('');
+//             $('#num-loads-now').val('');
+//             $('#num-loads-next-week').val('');
+//             $('#date-of-offering').val('');
+//             $('#valid-through-date').val('');
+//             $('#opportuinty-produce').val('');
+//             $('#shelf-life').val('');
+//             $('#box-offering').val('');
+//        })
+//        .catch((error) => {
+//          console.error("Error!", error.message);
+//          alert("Oh no... something went wrong! Please try again later!");
+//        });
+//    });
+//  });
  
  
